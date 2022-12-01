10b59a1140ec4a1a75accc87a54174e48f0c8783
Laggy6969 <99302452+Laggy6969@users.noreply.github.com>
2022-12-01 12:25:44 -0800

    Update Pair PE.py

--- Pair PE.py
+++ Pair PE.py
@@ -1,18 +1,33 @@
 import turtle as trtl
+import keyboard
 
-pnt = trtl.Turtle
+pnt = trtl.Turtle()
 
-#code here.....
+#----Constants----
+
+
+#----Functions----
+def callback(event):
+    userKey = str(event.name)
+    if userKey == 'w':
+        pnt.forward(5)
+
+
+
+#----Execution----
+keyboard.on_release(callback)
+keyboard.wait('esc')
+
 
 wn = trtl.Screen()
 wn.mainloop()
 
 
-
 """ TODO:  
-        -Make Tree & star
+        -Make Tree & star (Aiden)
         -Snow w/ button (Caden)
         -Name w/ box 
         -Presents
         -Godly message
+        -Key down events
         """
