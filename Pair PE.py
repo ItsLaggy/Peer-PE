4fef2a5e401c4ec09037cebc73cb8311b65dee83
Laggy6969 <99302452+Laggy6969@users.noreply.github.com>
2022-12-01 12:01:39 -0800

    Update Pair PE.py

--- Pair PE.py
+++ Pair PE.py
@@ -6,3 +6,12 @@ pnt = trtl.Turtle
 
 wn = trtl.Screen()
 wn.mainloop()
+
+
+""" TODO:  
+        -Make Tree & star
+        -Snow w/ button (Caden)
+        -Name w/ box 
+        -Presents
+        -Godly message
+        """
