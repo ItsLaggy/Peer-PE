04bcef656cbc9dc8accad4f626a790e3bdaac764
Laggy6969 <99302452+Laggy6969@users.noreply.github.com>
2022-12-02 12:24:34 -0800

    Update Pair PE.py

--- Pair PE.py
+++ Pair PE.py
@@ -17,7 +17,25 @@ USER_MOVEMENT, USER_TURN = 15, 15
 FLAKE_AMOUNT, FLAKE_COLOR, REFRESH_TIMER = 30, 'white', 30
 X_MIN, X_MAX, Y_MIN, Y_MAX = -400, 400, 300, 400
 YMIN_END, YMAX_END = -350, -400
-
+#           ___Tree Constants__
+CENTER_X= 0
+CENTER_Y= -300
+FIRST_TRIANGLE_X = -110
+SECOND_TRIANGLE_X = -100
+THIRD_TRIANGLE_X = -90
+FOURTH_TRIANGLE_X = -80
+FIRST_TRIANGLE_Y = -272
+SECOND_TRIANGLE_Y = -190
+THIRD_TRIANGLE_Y = - 100
+FOURTH_TRIANGLE_Y = -20
+#
+# #---- leaves of the tree size ----
+FIRST_LEAVES = 200
+SECOND_LEAVES = 180
+THIRD_LEAVES = 160
+FOUTH_LEAVES = 140
+TREE_COLOR = "green"
+TRUNK_COLOR = "brown"
 #----Variables----
 snowflakes = []
 current_x = []
@@ -76,37 +94,23 @@ def Update_Complete():
     #global showsnow
     Update_Position()
     wn.update()
-   
     wn.ontimer(Update_Complete, REFRESH_TIMER)
     wn.ontimer(Draw_Snow, REFRESH_TIMER)
     #Draw_Snow()
 #---- leaves of the tree size ----
-First_leaves = 200
-Second_leaves = 180
-Third_leaves = 160
-Fourth_leaves = 140
+
 #---- base of the tree vaule( bottom center of the canvas ) ----
-center_x= 0
-cenetr_y= -300
-first_triangle_x = -110
-second_triangle_x = -100
-third_triangle_x = -90
-fourth_triangle_x = -80
-first_triangle_y = -272
-second_triangle_y = -190
-third_triangle_y = - 100
-fourth_triangle_y = -20
+
 #---- tree colors ----
-tree_color = "green"
-trunk_color = "brown"
+
 #---- creates the tree trunk and trasports the turtle to the bottom center of the canvas ----
 def make_tree_trunk():
     pnt.speed(0)
     pnt.penup()
-    pnt.goto(center_x,cenetr_y)
+    pnt.goto(CENTER_X,CENTER_Y)
     pnt.pendown()
     pnt.left(45)
-    pnt.fillcolor(trunk_color)
+    pnt.fillcolor(TRUNK_COLOR)
     pnt.begin_fill()
     pnt.circle(20,360,4)
     pnt.end_fill()
@@ -114,7 +118,7 @@ def make_tree_trunk():
 def make_triangle():
     pnt.speed(0)
     pnt.begin_fill()
-    pnt.fillcolor(tree_color)
+    pnt.fillcolor(TREE_COLOR)
     pnt.right(45)
     pnt.forward(200)
     pnt.right(240)
@@ -126,62 +130,62 @@ def make_triangle():
 def postion():
     pnt.speed(0)
     pnt.penup()
-    pnt.goto(first_triangle_x,first_triangle_y)
+    pnt.goto(FIRST_TRIANGLE_X,FIRST_TRIANGLE_Y)
     pnt.pendown()
 #---- make tree leaves ----
 def tree_leaves():
     pnt.speed(0)
     pnt.begin_fill()
-    pnt.fillcolor(tree_color)
+    pnt.fillcolor(TREE_COLOR)
     pnt.right(45)
-    pnt.forward(First_leaves)
+    pnt.forward(FIRST_LEAVES)
     pnt.right(240)
-    pnt.forward(First_leaves)
+    pnt.forward(FIRST_LEAVES)
     pnt.right(240)
-    pnt.forward(First_leaves)
+    pnt.forward(FIRST_LEAVES)
     pnt.end_fill()
     pnt.penup()
     pnt.left(120)
-    pnt.goto(second_triangle_x,second_triangle_y)
+    pnt.goto(SECOND_TRIANGLE_X,SECOND_TRIANGLE_Y)
     pnt.pendown()
     pnt.begin_fill()
-    pnt.fillcolor(tree_color)
-    pnt.forward(Second_leaves)
+    pnt.fillcolor(TREE_COLOR)
+    pnt.forward(SECOND_LEAVES)
     pnt.right(240)
-    pnt.forward(Second_leaves)
+    pnt.forward(SECOND_LEAVES)
     pnt.right(240)
-    pnt.forward(Second_leaves)
+    pnt.forward(SECOND_LEAVES)
     pnt.end_fill()
     
     pnt.penup()
     pnt.left(120)
-    pnt.goto(third_triangle_x,third_triangle_y)
+    pnt.goto(THIRD_TRIANGLE_X,THIRD_TRIANGLE_Y)
     pnt.pendown()
     pnt.begin_fill()
-    pnt.fillcolor(tree_color)
-    pnt.forward(Third_leaves)
+    pnt.fillcolor(TREE_COLOR)
+    pnt.forward(THIRD_LEAVES)
     pnt.right(240)
-    pnt.forward(Third_leaves)
+    pnt.forward(THIRD_LEAVES)
     pnt.right(240)
-    pnt.forward(Third_leaves)
+    pnt.forward(THIRD_LEAVES)
     pnt.end_fill()
     pnt.penup()
     pnt.left(120)
-    pnt.goto(fourth_triangle_x,fourth_triangle_y)
+    pnt.goto(FOURTH_TRIANGLE_X,FOURTH_TRIANGLE_Y)
     pnt.pendown()
     pnt.begin_fill()
-    pnt.fillcolor(tree_color)
-    pnt.forward(Fourth_leaves)
+    pnt.fillcolor(TREE_COLOR)
+    pnt.forward(FOUTH_LEAVES)
     pnt.right(240)
-    pnt.forward(Fourth_leaves)
+    pnt.forward(FOUTH_LEAVES)
     pnt.right(240)
-    pnt.forward(Fourth_leaves)
+    pnt.forward(FOUTH_LEAVES)
     pnt.end_fill()
 #---- makes star for the tree ----
 def make_star():
     pnt.speed(0)
     pnt.penup()
-    pnt.goto(center_x+4 ,95)
+    pnt.goto(CENTER_X+4 ,95)
     pnt.pendown()
     pnt.color("yellow")
     pnt.begin_fill()
@@ -193,6 +197,12 @@ def make_star():
     pnt.penup()
     pnt.end_fill()
     pnt.hideturtle()
+def Tree_Creation():
+    make_tree_trunk()
+    postion()
+    tree_leaves()
+    make_star()
+
     
 
 
@@ -204,10 +214,7 @@ wn.bgpic('./Snow.gif')
 wn.addshape('./Snowflake.gif')
 
 #---- prints the code ----
-make_tree_trunk()
-postion()
-tree_leaves()
-make_star()
+Tree_Creation()
 
 #----Keydown Movement----
 keyboard.on_press_key(FORWARD_KEY, Forward_KeyDown)
