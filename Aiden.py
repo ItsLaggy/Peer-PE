8dff4e76d2cbf928c0963c74fdceb486cf4c1d81
Aidenpelligreen <119627992+Aidenpelligreen@users.noreply.github.com>
2023-02-08 17:07:52 -0800

    Update Aiden.py

--- Aiden.py
+++ Aiden.py
@@ -219,3 +219,29 @@ Tree_Creation()
 pnt.hideturtle()
 
 wn.mainloop()
+
+
+def monitor():
+  try:
+    
+    val1 = 17
+    val2 = 12
+
+    alkilines = list(range(val1, val2+1))
+
+    current = get_alkalinity()
+    mesg = "Alkalinity OK"
+
+    if (current < alkilines[0]):
+      mesg = "Alkalinity too low!"
+    elif (current > alkilines[5]):
+      mesg = "Alkalinity too high!"
+    
+  except:
+    print("Unexpected error") 
+    
+  return mesg
+
+# Function to simulate actual fish tank monitoring
+def get_alkalinity():
+  return 9
