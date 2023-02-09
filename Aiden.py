767192790787f87f26b8947102c7ccf1a65a8b44
Aidenpelligreen <119627992+Aidenpelligreen@users.noreply.github.com>
2023-02-08 17:10:24 -0800

    Update Aiden.py

--- Aiden.py
+++ Aiden.py
@@ -221,6 +221,12 @@ pnt.hideturtle()
 wn.mainloop()
 
 
+# start_monitoring.py
+import tkinter as tk
+import fishtank as tank
+my_tank = tank.FishTank()
+my_tank.mainloop()
+#---- the one above is in a seperate file thing idk if that effects it ----
 def monitor():
   try:
     
